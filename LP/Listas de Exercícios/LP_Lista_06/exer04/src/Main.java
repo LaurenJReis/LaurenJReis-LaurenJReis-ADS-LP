f6a0319a9d902877d79b681312001710b4e85d9e
LaurenJReis <laurenjuliareissilva@hotmail.com>
2025-06-13 22:26:05 -0300

    Exercicios

--- LP/Listas de Exercícios/LP_Lista_06/exer04/src/Main.java
+++ LP/Listas de Exercícios/LP_Lista_06/exer04/src/Main.java
@@ -2,3 +2,34 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Digite uma palavra ou frase: ");
+        String texto = scanner.nextLine();
+
+        if (isPalindromo(texto)) {
+            System.out.println("\"" + texto + "\" é um palíndromo!");
+        } else {
+            System.out.println("\"" + texto + "\" não é um palíndromo.");
+        }
+
+        scanner.close();
+    }
+
+    public static boolean isPalindromo(String texto) {
+        // Remove espaços e coloca tudo em minúsculo para ignorar diferença de maiúsculas/minúsculas e espaços
+        String textoLimpo = texto.replaceAll("\\s+", "").toLowerCase();
+
+        int inicio = 0;
+        int fim = textoLimpo.length() - 1;
+
+        while (inicio < fim) {
+            if (textoLimpo.charAt(inicio) != textoLimpo.charAt(fim)) {
+                return false;
+            }
+            inicio++;
+            fim--;
+        }
+        return true;
+    }
+}
